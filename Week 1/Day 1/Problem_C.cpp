b41805ac3e0b077c1d94ff402b9ef65cd4598b2c
Mehedi Hassan <124517067+mehedi2256@users.noreply.github.com>
2024-10-27 22:05:18 +0600

    Topic problem 3

---
+++ Week 1/Day 1/Problem_C.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define lli long long int
+#define B begin()
+#define E end()
+#define vi vector<int>
+#define pi pair<int, int>
+#define vpi vector<pair<int, int>>
+#define all(x) x.B, x.E
+#define fast() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+using namespace std;
+
+void mhsol()
+{
+    int n;
+    cin >> n;
+    set<pair<string, string>> st;
+    while (n--)
+    {
+        string s, sp;
+        cin >> s >> sp;
+        st.insert({s, sp});
+    }
+    cout << st.size() << endl;
+}
+int main()
+{
+    fast();
+    int t = 1;
+    // cin >> t;
+    while (t--)
+        mhsol();
+    return 0;
+}
