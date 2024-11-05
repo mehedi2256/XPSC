81fa44bea164ae2ef9cfe1c939000856f57f9dc0
Mehedi Hassan <124517067+mehedi2256@users.noreply.github.com>
2024-11-05 22:53:56 +0600

    Day 3

---
+++ Week 2/Day 3/Problem_2.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define lli long long int
+#define B begin()
+#define E end()
+#define vi vector<int>
+#define pi pair<int, int>
+#define vpi vector<pair<int, int>>
+#define all(x) x.B, x.E
+#define fast() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+using namespace std;
+
+void mhsol()
+{
+    int n;
+    cin >> n;
+    int cnt = 1;
+    vi v(n);
+    for (auto &it : v)
+        cin >> it;
+    sort(all(v));
+    for (int i = 0; i < n; i++)
+        if (v[i] >= cnt)
+            cnt++;
+    cout << cnt - 1 << endl;
+}
+int main()
+{
+    fast();
+    int t = 1;
+    // cin >> t;
+    while (t--)
+        mhsol();
+    return 0;
+}
